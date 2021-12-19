7f4525892a1bca5280a55d1290ff05e56b43acf5
DataAIMiner <75566147+DataAiMiner@users.noreply.github.com>
2021-12-19 12:57:52 +0900

    Create 5957. Adding Spaces to a String.py
    tried so many times..

---
+++ Weekly Contest 272/5957. Adding Spaces to a String.py
@@ -0,0 +1,25 @@
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        # str_list = list(s)
+        # for i in range(len(spaces)):
+        #     str_list.insert(spaces[i]+i, ' ')
+        # return ''.join(str_list)
+        
+        # for i, position in enumerate(spaces):
+        #     str_list.insert(position + i, ' ')
+        # return ''.join(str_list)
+        
+        # for i in spaces:
+        #     s.replace(s[i], " "+s[i])[i-1: i]
+        #     print(s)
+        # return s
+        
+        # loop = 0
+        # for i in spaces:
+        #     s = s[0: i+loop] + ' ' + s[i+loop:]
+        #     loop += 1
+        # return s
+        
+        parts = [s[i:j] for i,j in zip([0]+spaces, spaces[0:]+[None])]
+        return ' '.join(parts)
+                
