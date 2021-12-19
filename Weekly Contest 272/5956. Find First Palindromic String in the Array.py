7b4a9f5d63869e17a9acb39061e77be25a99f54b
DataAIMiner <75566147+DataAiMiner@users.noreply.github.com>
2021-12-19 13:13:05 +0900

    Update 5956. Find First Palindromic String in the Array.py
    better easier solution

--- Weekly Contest 272/5956. Find First Palindromic String in the Array.py
+++ Weekly Contest 272/5956. Find First Palindromic String in the Array.py
@@ -1,25 +1,32 @@
 class Solution:
     def firstPalindrome(self, words: List[str]) -> str:
-        for i, word in enumerate(words):
-            chars = list(word)
+#         for i, word in enumerate(words):
+#             chars = list(word)
             
-            low, high = 0, len(chars)-1
-            if len(chars) % 2 == 0:
-                while low < high:
-                    if chars[low] != chars[high]:
-                        break
-                    else:
-                        low, high = low+1, high-1
-                if chars[low] == chars[high] and high == low-1:
-                    return word
-            else:
-                mid = (low + high) // 2
-                while low != high:
-                    if chars[low] != chars[high]:
-                        break
-                    else:
-                        low, high = low+1, high-1
-                if low == mid and high == mid:
-                    return word
-       
-        return ""
+#             low, high = 0, len(chars)-1
+#             if len(chars) % 2 == 0:
+#                 while low < high:
+#                     if chars[low] != chars[high]:
+#                         break
+#                     else:
+#                         low, high = low+1, high-1
+#                 if chars[low] == chars[high] and high == low-1:
+#                     return word
+#             else:
+#                 mid = (low + high) // 2
+#                 while low != high:
+#                     if chars[low] != chars[high]:
+#                         break
+#                     else:
+#                         low, high = low+1, high-1
+#                 if low == mid and high == mid:
+#                     return word
+#         return ""
+    
+        for word in words:
+            if word == word[::-1]:
+                return word
+        else:
+            return ""
+            
+
