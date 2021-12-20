6dacafbecaea6ece1d55ee0637554a1b7cd0ca6d
DataAIMiner <75566147+DataAiMiner@users.noreply.github.com>
2021-12-20 22:04:50 +0900

    Update 2110. Number of Smooth Descent Periods of a Stock.py
    added explanation

--- Weekly Contest 272/2110. Number of Smooth Descent Periods of a Stock.py
+++ Weekly Contest 272/2110. Number of Smooth Descent Periods of a Stock.py
@@ -6,6 +6,8 @@ class Solution:
         n = len(prices)
         result, current = 0, 1
 
+        # 소그룹으로 묶을 수 있는 것들(if)와 개별로 노는 것(else)로 나눌 수 있다.
+        # 소그룹을 이루는 원소의 수가 N이면, 해당 소그룹에서 나올 수 있는 모든 경우의 수는 N(N+1)/2 가지
         for i in range(1, n):
             if prices[i] == prices[i - 1] - 1:
                 current += 1
