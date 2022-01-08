c757871ceb3a35c66f01c3975b0713439a859ba4
DataAIMiner <75566147+DataAiMiner@users.noreply.github.com>
2022-01-08 23:50:19 +0900

    Update 141. Linked List Cycle.py
    added failed solution

--- 141. Linked List Cycle.py
+++ 141. Linked List Cycle.py
@@ -17,3 +17,30 @@ class Solution:
 #             head.val = None
 #             head = head.next
 #         return False
+
+
+## Failed One
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:
+#         nodes = []
+#         curr = head
+#         prev = head
+#         check_point = -1
+#         while True:
+#             if curr is None:
+#                 return False
+            
+#             if curr.val in nodes:
+#                 check_point = nodes.index(curr.val)
+#                 print(check_point)
+#                 print(nodes)
+#                 print(curr.val, prev.val)
+#                 if prev.val == nodes[check_point-1] or (prev.val == curr.val and curr.next is None):
+#                     return True
+#                 else:
+#                     nodes.append(curr.val)
+#             else:
+#                 nodes.append(curr.val)
+                
+#             prev = curr
+#             curr = curr.next
